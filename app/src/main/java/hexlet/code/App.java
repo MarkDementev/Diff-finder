d5b7e5511cd69e7a9e41534231643ad9f82ab2be
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-01-31 17:19:40 +0300

    do 3d step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
+        return 0;
     }
 }
