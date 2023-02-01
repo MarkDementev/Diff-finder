499aeb05cac834f5ac655e50b6e17defce4e25e3
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-02-01 15:28:52 +0300

    do 4th step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,24 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    int format;
+
+    @Parameters(paramLabel = "filePath1", index = "0", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(paramLabel = "filePath2", index = "1", description ="path to second file")
+    private String filePath2;
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
