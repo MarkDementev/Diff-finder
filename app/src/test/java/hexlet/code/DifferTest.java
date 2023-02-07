384bc94e575e89c0afa1fadc55ea593d14430af1
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-02-07 18:15:31 +0300

    make basic tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,14 +2,10 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatException;
 
 public class DifferTest {
-    //дефолтное - работает OK
-    //второй тест - тоже дефолтный придумай OK
-    // первого паф нет
-    // второго паф нет
-    // обоих паф нет
-    private String differTestNormalFirstCorrectString = "\n{\n"
+    private String differTestFirstCorrectString = "\n{\n"
             + " - follow: false\n"
             + "   host: hexlet.io\n"
             + " - proxy: 123.234.53.22\n"
@@ -18,7 +14,7 @@ public class DifferTest {
             + " + verbose: true\n"
             + "}";
 
-    private String differTestNormalSecondCorrectString = "\n{\n"
+    private String differTestSecondCorrectString = "\n{\n"
             + "   A: A\n"
             + "   B: B\n"
             + " - C: true\n"
@@ -27,19 +23,66 @@ public class DifferTest {
             + " + E: true\n"
             + "}";
 
+    private String differTestFirstEmptyPathCorrectString = "\n{\n"
+            + " + follow: false\n"
+            + " + host: hexlet.io\n"
+            + " + proxy: 123.234.53.22\n"
+            + " + timeout: 50\n"
+            + "}";
+
+    private String differTestSecondEmptyPathCorrectString = "\n{\n"
+            + " - A: A\n"
+            + " - B: B\n"
+            + " - C: true\n"
+            + " - D: 1\n"
+            + "}";
+
+    private String differTestBothEmptyPathsCorrectString = "\n{\n}";
+
     @Test
-    public void differTestNormalFirst() throws Exception {
-        String firstPath = "./src/test/resources/fixtures/differTestNormalFirstPath1.json";
-        String secondPath = "./src/test/resources/fixtures/differTestNormalFirstPath2.json";
+    public void differTestFirst() throws Exception {
+        String firstPath = "./src/test/resources/fixtures/differTestFirstPath1.json";
+        String secondPath = "./src/test/resources/fixtures/differTestFirstPath2.json";
         String result = Differ.generate(firstPath, secondPath);
-        assertThat(result).isEqualTo(differTestNormalFirstCorrectString);
+        assertThat(result).isEqualTo(differTestFirstCorrectString);
     }
 
     @Test
-    public void differTestNormalSecond() throws Exception {
-        String firstPath = "./src/test/resources/fixtures/differTestNormalSecondPath1.json";
-        String secondPath = "./src/test/resources/fixtures/differTestNormalSecondPath2.json";
+    public void differTestSecond() throws Exception {
+        String firstPath = "./src/test/resources/fixtures/differTestSecondPath1.json";
+        String secondPath = "./src/test/resources/fixtures/differTestSecondPath2.json";
         String result = Differ.generate(firstPath, secondPath);
-        assertThat(result).isEqualTo(differTestNormalSecondCorrectString);
+        assertThat(result).isEqualTo(differTestSecondCorrectString);
+    }
+
+    @Test
+    public void differTestFirstEmptyPath() throws Exception {
+        String firstPath = "./src/test/resources/fixtures/emptyFile.json";
+        String secondPath = "./src/test/resources/fixtures/differTestFirstPath1.json";
+        String result = Differ.generate(firstPath, secondPath);
+        assertThat(result).isEqualTo(differTestFirstEmptyPathCorrectString);
+    }
+
+    @Test
+    public void differTestSecondEmptyPath() throws Exception {
+        String firstPath = "./src/test/resources/fixtures/differTestSecondPath1.json";
+        String secondPath = "./src/test/resources/fixtures/emptyFile.json";
+        String result = Differ.generate(firstPath, secondPath);
+        assertThat(result).isEqualTo(differTestSecondEmptyPathCorrectString);
+    }
+
+    @Test
+    public void differTestBothEmptyPaths() throws Exception {
+        String firstPath = "./src/test/resources/fixtures/emptyFile.json";
+        String secondPath = "./src/test/resources/fixtures/emptyFile.json";
+        String result = Differ.generate(firstPath, secondPath);
+        assertThat(result).isEqualTo(differTestBothEmptyPathsCorrectString);
+    }
+
+    @Test
+    public void differTestNoFile() {
+        String firstPath = "./src/test/resources/fixtures/this file is fantasy.json";
+        String secondPath = "./src/test/resources/fixtures/differTestFirstPath1.json";
+        assertThatException().isThrownBy(() -> Differ.generate(firstPath, secondPath));
     }
 }
